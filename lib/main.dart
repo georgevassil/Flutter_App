1a31b2dc0725280c48dba37da442b79ab6e151ab
georgevassil <89745196+georgevassil@users.noreply.github.com>
2025-10-08 15:08:47 +0300

    Update app title for workflow test(pt2) (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Learning the Git Workflow',
+      title: 'Learning the Git Workflow AGAIN (PT2)',
       theme: ThemeData(
         // This is the theme of your application.
         //
